e4cc6c25311a142e0d29c455cf3858274d5301ce
Katherine Blaine <blaine.katherine12@gmail.com>
2022-12-17 17:22:12 -0600

    Delete comments and clean-up spacing

--- main.js
+++ main.js
@@ -1,46 +1,29 @@
-// Variables -
-
-// buttons -
-
 var receiveMessageButton = document.querySelector('#receive-message-button')
 var viewSavedButton = document.querySelector('#view-saved-messages')
 var homeButton = document.querySelector('#home-button')
 
-
-// also inserting home and favorite message button below - total of 4 buttons
-
-// main page sections -
 var textBoxOne = document.querySelector('.textBox1')
 var textBoxTwo = document.querySelector('.textBox2')
 var mainPage = document.querySelector('#main-page')
 
-// main page section 1 elements -
 var affirmationRadio = document.querySelector('#affirmation-selector')
 var mantraRadio = document.querySelector('#mantra-selector')
 
-//main page section 2 elements -
-// var messageBox = document.querySelector('.message')
 var message
 
-// view saved messages section:
 var savedMessageSection = document.querySelector('.view-saved-messages')
 var favoriteMessages = document.querySelector('.saved-messages')
 var savedMessage = document.querySelector('#saved-message')
 var savedMessages= [];
 var messageBox = document.querySelector('.message-box')
 
-// Event Listeners -
 
-// Displays random message when receive message button is clicked
 receiveMessageButton.addEventListener('click', displayMessage)
 
-// displays saved message page when view-saved messages button is clicked
 viewSavedButton.addEventListener('click', showSavedMessages)
 
 homeButton.addEventListener('click', function(){
     returnHome()})
-
-// Event Handlers -
 
 function displayMessage() {
     if (affirmationRadio.checked === true) {
@@ -65,7 +48,6 @@ function saveMessage(message) {
     savedMessages.push(message)
     }
 
-
 function returnHome() {
     hide(savedMessageSection)
     show(mainPage)
@@ -79,37 +61,6 @@ function showSavedMessages() {
     displaySavedMessages()
     }
 
-// function displaySavedMessages() {
-//     favoriteMessages.innerHTML = '';
-//     for (var i = 0; i < savedMessages.length; i++) {
-//         favoriteMessages.innerHTML += `
-//         <section class ='message-box'>
-//             <article class='saved-message'>${savedMessages[i]}</article>
-//             <button class="remove-saved-message-button">X</button>
-//         </section>`
-//         var savedMessage = document.querySelector('.saved-message')
-//         var deleteMessageButton = document.querySelector('.remove-saved-message-button')
-//         deleteMessageButton.addEventListener('click', function() {
-//         removeMessage(savedMessage.innerText)})
-//     } show(favoriteMessages)
-// }
-
-// function displaySavedMessages() {
-//     favoriteMessages.innerHTML = '';
-//     for (var i = 0; i < savedMessages.length; i++) {
-//         favoriteMessages.innerHTML += `
-//         <section class ='message-box'>
-//             <article class='saved-message'>${savedMessages[i]}</article>
-//             <button class="remove-saved-message-button">X</button>
-//         </section>`
-//         var removeMessageButtons = document.querySelectorAll('.remove-saved-message-button')
-//         removeMessageButtons.forEach(element => {
-//             element.addEventListener('click', function() {
-//                 removeMessage(savedMessages[i])})
-//         })
-//     } show(favoriteMessages)
-// }
-
 function displaySavedMessages() {
     favoriteMessages.innerHTML = '';
     for (var i = 0; i < savedMessages.length; i++) {
@@ -118,39 +69,16 @@ function displaySavedMessages() {
             <article id='saved-message'>${savedMessages[i]}</article>
             <button id="remove-saved-message-button">X</button>
         </section>`
+        var deleteMessageButton = document.querySelector('#remove-saved-message-button')
+        deleteMessageButton.addEventListener('click', function() {
+        removeMessage(message)})
     } show(favoriteMessages)
-    var deleteMessageButton = document.querySelector('#remove-saved-message-button')
-    deleteMessageButton.addEventListener('click', function() {
-    removeMessage(message)})
 }
-
-// why is there not any event listeners for the buttons below???
 
 function removeMessage(messegeToDelete) {
     savedMessages.splice(savedMessages[savedMessages.indexOf(messegeToDelete)], 1)
     displaySavedMessages()}
 
-// function removeMessage(messageToDelete) {
-//     console.log(event.target.parentNode)
-//     console.log(messageToDelete[0].innerHTML)
-// }
-// find the corresponding text box
-// event.target.parentNode
-
-// find the inner text of that text box
-// messageToDelete[0].innerHTML
-
-// find the index position of that string value in the array
-//splice that array value
-    // savedMessages.splice(savedMessages[savedMessages.indexOf(messageToDelete)], 1)
-    // displaySavedMessages()}
-
-// function removeMessage(messageToDelete) {
-//     console.log(messageToDelete)
-//     savedMessages.splice(savedMessages[savedMessages.indexOf(messageToDelete)], 1)
-//     displaySavedMessages()}
-
-// Misc. Functions -
 
 function getRandomIndex(array) {
     return Math.floor(Math.random() * array.length)
